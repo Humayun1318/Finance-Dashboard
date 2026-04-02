325aed69a90aab82e38e8da3f06f1cbbe6759d6b
Humayun Kabir <humayunkabir6267@gmail.com>
2026-04-03 02:24:16 +0600

    Feat: project ready to start with all dependencies installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-
-import './App.css'
+import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
- 
-
   return (
     <>
       <h1>This is the Finance Dashboard</h1>
+      <Button>
+        Click Me
+      </Button>
     </>
   );
 }
 
-export default App
+export default App;
